7f3a6f7b90f437b212049eb74d10013366190b2f
Tim <tim@lu.de>
2022-05-18 18:15:28 +0200

    Added Tests

---
+++ backend/src/test/java/de/neuefische/backend/controller/StockControllerTest.java
@@ -0,0 +1,52 @@
+package de.neuefische.backend.controller;
+
+import de.neuefische.backend.model.Stock;
+import de.neuefische.backend.repository.StockRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class StockControllerTest {
+
+    @Autowired
+    private StockRepo stockRepo;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private WebTestClient testClient;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void addNewStock() {
+        //GIVEN
+        Stock stock = Stock.builder().name("Apple").symbol("AAPL").amountOfShares(10).build();
+
+        //WHEN
+        Stock actual = testClient.post()
+                .uri("stock")
+                .bodyValue(stock)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Stock.class)
+                .returnResult()
+                .getResponseBody();
+
+        //Then
+        assertNotNull(actual);
+        assertNotNull(actual.getId());
+        Stock expected = Stock.builder().id(actual.getId()).name("Apple").symbol("AAPL").amountOfShares(10).build();
+        assertEquals(expected, actual);
+    }
+}
---
+++ backend/src/test/java/de/neuefische/backend/service/StockServiceTest.java
@@ -0,0 +1,42 @@
+package de.neuefische.backend.service;
+
+import de.neuefische.backend.dto.CreateStock;
+import de.neuefische.backend.model.Stock;
+import de.neuefische.backend.repository.StockRepo;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class StockServiceTest {
+
+    private final StockRepo stockRepo = mock(StockRepo.class);
+
+    private final StockService stockService = new StockService(stockRepo);
+
+    @Test
+    void addNewStock() {
+        //GIVEN
+        Stock stockToInsert = Stock.builder().name("Apple").symbol("AAPL").amountOfShares(10).build();
+        when(stockRepo.insert(stockToInsert)).thenReturn(Stock.builder().id("123456").name("Apple").symbol("AAPL").amountOfShares(10).build());
+
+        //WHEN
+        CreateStock newStock = CreateStock.builder().name("Apple").symbol("AAPL").amountOfShares(10).build();
+        Stock actual = stockService.addNewStock(newStock);
+
+        //THEN
+        Stock expected = Stock.builder().id("123456").name("Apple").symbol("AAPL").amountOfShares(10).build();
+        verify(stockRepo).insert(stockToInsert);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void addNewStock_whenAmountOfShares_isNotValid_shouldThrowException() {
+        //GIVEN
+        CreateStock newStock = CreateStock.builder().name("Apple").symbol("AAPL").amountOfShares(0).build();
+
+        //THEN
+        assertThrows(IllegalArgumentException.class, () -> stockService.addNewStock(newStock));
+    }
+}
