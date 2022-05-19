ebf4df147c5acb96707f6b6e7c2a1a57471e1a3f
TimLucas16 <102078298+TimLucas16@users.noreply.github.com>
2022-05-19 11:10:00 +0200

    Update StockServiceTest.java
    CreateStock changed to CreateStockDto

--- backend/src/test/java/de/neuefische/backend/service/StockServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/service/StockServiceTest.java
@@ -1,6 +1,6 @@
 package de.neuefische.backend.service;
 
-import de.neuefische.backend.dto.CreateStock;
+import de.neuefische.backend.dto.CreateStockDto;
 import de.neuefische.backend.model.Stock;
 import de.neuefische.backend.repository.StockRepo;
 import org.junit.jupiter.api.Test;
@@ -22,7 +22,7 @@ class StockServiceTest {
         when(stockRepo.insert(stockToInsert)).thenReturn(Stock.builder().id("123456").name("Apple").symbol("AAPL").amountOfShares(10).build());
 
         //WHEN
-        CreateStock newStock = CreateStock.builder().name("Apple").symbol("AAPL").amountOfShares(10).build();
+        CreateStockDto newStock = CreateStockDto.builder().name("Apple").symbol("AAPL").amountOfShares(10).build();
         Stock actual = stockService.addNewStock(newStock);
 
         //THEN
@@ -34,7 +34,7 @@ class StockServiceTest {
     @Test
     void addNewStock_whenAmountOfSharesIsZeroOrLess_shouldThrowException() {
         //GIVEN + WHEN
-        CreateStock newStock = CreateStock.builder().name("Apple").symbol("AAPL").amountOfShares(0).build();
+        CreateStockDto newStock = CreateStockDto.builder().name("Apple").symbol("AAPL").amountOfShares(0).build();
 
         //THEN
         assertThrows(IllegalArgumentException.class, () -> stockService.addNewStock(newStock));
