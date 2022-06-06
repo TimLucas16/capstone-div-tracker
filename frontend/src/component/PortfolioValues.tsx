c819b91ef53b3f6ccd50f7c575485822f29fa371
Tim <tim@lu.de>
2022-06-06 19:46:44 +0200

    styled stockCard

--- frontend/src/component/PortfolioValues.tsx
+++ frontend/src/component/PortfolioValues.tsx
@@ -1,15 +1,22 @@
 import {Portfolio} from "../model/Portfolio";
+
 type PortfolioProps = {
-    pfValues : Portfolio
+    pfValues: Portfolio
 }
 
-export default function PortfolioValues({pfValues} : PortfolioProps) {
+export default function PortfolioValues({pfValues}: PortfolioProps) {
 
     return (
-        <div className={"pfValues"}>
-            <div>{(pfValues.pfValue / 100).toFixed(2)} $</div>
-            <div>{(pfValues.pfTotalReturnAbsolute / 100).toFixed(2)} $</div>
-            <div>{(pfValues.pfTotalReturnPercent).toFixed(2)} %</div>
+        <div className={"pfValues-container"}>
+            <div>
+                <div className={"pfTitel"}>Portfolio Value</div>
+                <div className={"pfValues"}>{(pfValues.pfValue / 100).toFixed(2)} $</div>
+            </div>
+            <div>
+                <div className={"pfTitel"}>Total Return</div>
+                <div className={"pfValues"}>{(pfValues.pfTotalReturnAbsolute / 100).toFixed(2)} $</div>
+                <div className={"pfValues"}>{(pfValues.pfTotalReturnPercent).toFixed(2)} %</div>
+            </div>
         </div>
     )
 }
