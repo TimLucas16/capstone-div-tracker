be2824a47ef06f14ab4537fa78426a0764f5b29b
Tim <tim@lu.de>
2022-06-10 15:46:19 +0200

    styling NavBar and NewStockPage

--- frontend/src/component/NavBar.tsx
+++ frontend/src/component/NavBar.tsx
@@ -1,13 +1,20 @@
 import {Link} from "react-router-dom";
 import "../styles/NavBar.css";
-import logo from "../images/logo.png";
-import { BsFillPlusCircleFill } from "react-icons/bs";
+import logo from "../images/logo2.png";
+import {BsFillPlusCircleFill} from "react-icons/bs";
 
 export default function NavBar() {
     return (
         <div className={"navbar"}>
-            <Link to={"/"}><img className={"logo"} src={logo} alt="Logo"/></Link>
-            <Link to={"/addStock"}><BsFillPlusCircleFill className={"addStock"}/></Link>
+            <div>
+                <div className={"logo-container"}>
+                    <Link to={"/"}><img className={"logo"} src={logo} alt="Logo"/></Link>
+                    <div className={"logo-text"}>Portfolio-Tracker</div>
+                </div>
+                <div className={"addStock-container"}>
+                    <Link to={"/addStock"}><BsFillPlusCircleFill viewBox={"0 0 22 22"} className={"addStock1"}/></Link>
+                </div>
+            </div>
         </div>
     )
 }
